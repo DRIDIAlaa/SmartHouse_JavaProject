ead26313e34d45f12e940a68d8dc06eada0305f6
Five Consulting <projet@five-consulting.com>
2022-12-07 21:18:07 -0800

    new commit

---
+++ SmartHouse_JavaProject/src/Controllers/WidgetsController.java
@@ -0,0 +1,18 @@
+package Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.StackPane;
+
+public class WidgetsController {
+	@FXML 
+	private StackPane contentArea;
+
+	public WidgetsController() {
+		
+	}
+}
